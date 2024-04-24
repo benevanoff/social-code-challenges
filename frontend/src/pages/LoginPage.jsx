6a4b47fa8e2b53ef0c753b8b41864bc24387d3b0
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-04-24 10:39:49 +0800

    added register and login page

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "../components/LoginForm";
+
+const LoginPage = () => {
+  // Login or Register
+
+  return (
+    <div className="login-page">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
