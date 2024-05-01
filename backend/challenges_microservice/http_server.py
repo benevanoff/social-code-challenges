241fc617b4b1e4c34c1e3373bd86e3bdc4b51e7c
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-05-01 10:45:44 +0800

    filled out link project route

--- backend/challenges_microservice/http_server.py
+++ backend/challenges_microservice/http_server.py
@@ -25,17 +25,22 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["Content-Type", "Set-Cookie"]
+    allow_headers=["Content-Type", "Set-Cookie"],
 )
+
 
 # route handlers
 @app.get("/")
 def root():
     return "Hello Challenges"
 
-async def whoami(session_id:str):
+
+async def whoami(session_id: str):
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://users_microservice:8080/users/whoami', headers={'Cookie': f'session_id={session_id}'}) as response:
+        async with session.get(
+            "http://users_microservice:8080/users/whoami",
+            headers={"Cookie": f"session_id={session_id}"},
+        ) as response:
             whoami_response_code = response.status
             if whoami_response_code == 200:
                 whoami_response_json = await response.json()
@@ -54,28 +59,47 @@ async def get_challenges(sql_client=Depends(get_db)):
         query_result = await cur.fetchall()
     return query_result
 
+
 class CreateChallengeRequest(BaseModel):
     start_date: int
     end_date: int
     description: str
+
+
 @app.post("/challenges/create")
-async def create_challenge(request_body:CreateChallengeRequest, session_id:str=Cookie(None), sql_client=Depends(get_db)):
+async def create_challenge(
+    request_body: CreateChallengeRequest,
+    session_id: str = Cookie(None),
+    sql_client=Depends(get_db),
+):
     """
     Create a new challenge by specifying a challenge name, objective/description, a start date, and challenge end date.
 
     Only available to admin users.
     """
     whoami_response = await whoami(session_id)
-    if whoami_response.get('is_admin') != 1:
+    if whoami_response.get("is_admin") != 1:
         raise HTTPException(status_code=401)
     async with sql_client.cursor() as cur:
-        await cur.execute("INSERT INTO challenges (start_date, end_date, description) VALUES (%s,%s,%s)", 
-                          (datetime.datetime.fromtimestamp(request_body.start_date).replace(tzinfo=datetime.timezone.utc), 
-                            datetime.datetime.fromtimestamp(request_body.end_date).replace(tzinfo=datetime.timezone.utc), request_body.description))
+        await cur.execute(
+            "INSERT INTO challenges (start_date, end_date, description) VALUES (%s,%s,%s)",
+            (
+                datetime.datetime.fromtimestamp(request_body.start_date).replace(
+                    tzinfo=datetime.timezone.utc
+                ),
+                datetime.datetime.fromtimestamp(request_body.end_date).replace(
+                    tzinfo=datetime.timezone.utc
+                ),
+                request_body.description,
+            ),
+        )
     return 200
 
+
 @app.post("/challenges/register/{challenge_id}")
-async def register_for_challenge(challenge_id:int, session_id:str=Cookie(None), sql_client=Depends(get_db)):
+async def register_for_challenge(
+    challenge_id: int, session_id: str = Cookie(None), sql_client=Depends(get_db)
+):
     """
     A user may POST to this route before the challenge's start_date to register (aka signal intent to participate in) for an up coming challenge.
 
@@ -85,51 +109,76 @@ async def register_for_challenge(challenge_id:int, session_id:str=Cookie(None), 
     if not whoami_response:
         raise HTTPException(status_code=401)
     async with sql_client.cursor() as cur:
-        await cur.execute("INSERT INTO submissions (username, challenge_id) VALUES (%s,%s)", (whoami_response.get('username'), challenge_id))
+        await cur.execute(
+            "INSERT INTO submissions (username, challenge_id) VALUES (%s,%s)",
+            (whoami_response.get("username"), challenge_id),
+        )
     return 200
 
+
 @app.get("/challenges/registrations/{challenge_id}")
-async def get_challenge_registrations(challenge_id:int, sql_client=Depends(get_db)):
+async def get_challenge_registrations(challenge_id: int, sql_client=Depends(get_db)):
     """
     List all of the users who have registered for a challenge by username.
 
     Returns code 404 if the challenge_id is invalid.
     """
     async with sql_client.cursor() as cur:
-        await cur.execute("SELECT username FROM submissions WHERE challenge_id=%s",(challenge_id))
+        await cur.execute(
+            "SELECT username FROM submissions WHERE challenge_id=%s", (challenge_id)
+        )
         query_result = await cur.fetchall()
     if not query_result:
         return []
-    return [x['username'] for x in query_result]
+    return [x["username"] for x in query_result]
+
 
 class LinkProjectRequest(BaseModel):
     link: str
+
+
 @app.post("/challenges/submission/link_project/{submission_id}")
-async def link_project(submission_id:int, request_body:LinkProjectRequest):
+async def link_project(
+    submission_id: int, request_body: LinkProjectRequest, sql_client=Depends(get_db)
+):
     """
     Record the link to the code repository associated with the challenge submission
     in the `code_repository` column of the `submissions` database table.
     """
-    print("Path Param", submission_id)
-    print("Body", request_body)
+
+    async with sql_client.cursor() as cur:
+        await cur.execute(
+            "UPDATE submissions SET code_repository = %s WHERE submission_id = %s",
+            (request_body.link, submission_id),
+        )
+
     return 200
 
+
 @app.post("/challenges/submission/news/create/{submission_id}")
-async def post_news(submission_id:int):
+async def post_news(submission_id: int):
     """
     TODO
     """
     pass
 
+
 class VoteRequest(BaseModel):
     weight: Optional[int] = 1
+
+
 @app.post("/challenges/submission/vote/{submission_id}")
-async def vote(submission_id:int, request_body:VoteRequest=None, session_id:str=Cookie(None), sql_client=Depends(get_db)):
+async def vote(
+    submission_id: int,
+    request_body: VoteRequest = None,
+    session_id: str = Cookie(None),
+    sql_client=Depends(get_db),
+):
     """
-    Vote for a submission. Each user may place three votes per challenge, each with a different weight. 
-    
-    The weights act as multipliers. A vote with weight 3 gives 3 points while a vote with weight 1 only gives 1 point. 
-    
+    Vote for a submission. Each user may place three votes per challenge, each with a different weight.
+
+    The weights act as multipliers. A vote with weight 3 gives 3 points while a vote with weight 1 only gives 1 point.
+
     A user may use each weight only once per challenge.
     """
     whoami_response = await whoami(session_id)
@@ -138,19 +187,30 @@ async def vote(submission_id:int, request_body:VoteRequest=None, session_id:str=
         request_body = VoteRequest(weight=1)
     async with sql_client.cursor() as cur:
         # check if the user already voted for this submission
-        await cur.execute("SELECT vote_id FROM votes WHERE voter_username=%s", (voter_username))
-        if (await cur.fetchone()):
-            raise HTTPException(status_code=400, detail='You already voted for this submission')
-        await cur.execute("INSERT IGNORE INTO votes (submission_id, voter_username, weight) VALUES (%s,%s,%s)", (submission_id, voter_username, request_body.weight))
+        await cur.execute(
+            "SELECT vote_id FROM votes WHERE voter_username=%s", (voter_username)
+        )
+        if await cur.fetchone():
+            raise HTTPException(
+                status_code=400, detail="You already voted for this submission"
+            )
+        await cur.execute(
+            "INSERT IGNORE INTO votes (submission_id, voter_username, weight) VALUES (%s,%s,%s)",
+            (submission_id, voter_username, request_body.weight),
+        )
     return 200
 
+
 @app.get("/challenges/submissions/{challenge_id}")
-async def get_challenge_submissions(challenge_id:int, sql_client=Depends(get_db)):
+async def get_challenge_submissions(challenge_id: int, sql_client=Depends(get_db)):
     """
     List the top 10 submissions for a challenge
     """
     async with sql_client.cursor() as cur:
-        await cur.execute("SELECT submissions.submission_id, username, SUM(votes.weight) FROM submissions LEFT JOIN votes ON submissions.submission_id=votes.submission_id WHERE challenge_id=%s GROUP BY submissions.submission_id LIMIT 10",(challenge_id))
+        await cur.execute(
+            "SELECT submissions.submission_id, username, SUM(votes.weight) FROM submissions LEFT JOIN votes ON submissions.submission_id=votes.submission_id WHERE challenge_id=%s GROUP BY submissions.submission_id LIMIT 10",
+            (challenge_id),
+        )
         query_result = await cur.fetchall()
     if not query_result:
         raise HTTPException(status_code=404)
