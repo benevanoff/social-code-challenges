e470721644496f1d33c6e2e9ea1cdf017bfce526
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-05-11 22:10:01 +0800

    changed login button navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { redirect } from "react-router-dom";
 
 const Navbar = () => {
+
+  // TODO: Conditionally render login button if user is not logged in. Need SessionCookie
+
   return (
     <div className="navbar sticky">
       <h1 className="navbar-title">Social Code Challenges</h1>
@@ -9,7 +13,7 @@ const Navbar = () => {
         <Link to="/" className="navbar-challenges">
           Challenges
         </Link>
-        <button className="navbar-profile">T</button>
+        <Link to='/login' className="navbar-login">Login</Link>
       </nav>
     </div>
   );
